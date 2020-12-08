e1c1a3b33259084a7b1226f3871e835c6b5470c8
Robert Ryan <robert.ryan@mindspring.com>
2020-12-08 09:41:37 -0800

    Initial Commit

---
+++ DispatchGroupSubclass/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DispatchGroupSubclass
+//
+//  Created by Robert Ryan on 12/8/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
